68b1b26d8c5f021cd139fd3d920ea6915f123258
Vitalychez <support@retolife.ru>
2015-03-11 16:04:46 +0300

    Zend start

--- library/module/Application/config/module.config.php
+++ library/module/Application/config/module.config.php
@@ -8,50 +8,25 @@
  */
 
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/application',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
+	'router' => array(
+         'routes' => array(
+             'application' => array(
+                 'type'    => 'segment',
+                 'options' => array(
+                     'route'    => '/[:action][/:id]',
+                     'constraints' => array(
+                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                         'id'     => '.*?',
+                     ),
+                     'defaults' => array(
+                         'controller' => 'Application\Controller\Index',
+                         'action'     => 'index',
+                     ),
+                 ),
+             ),
+         ),
+     ),
+	
     'service_manager' => array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
@@ -83,8 +58,15 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layouts/layout'           => __DIR__ . '/../view/layouts/layout.phtml',
+
+			'application/index/users' => __DIR__ . '/../view/application/index/users.phtml',
+			'application/index/topic' => __DIR__ . '/../view/application/index/topic.phtml',
+			'application/index/settings' => __DIR__ . '/../view/application/index/settings.phtml',
 			'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+			'application/index/forums' => __DIR__ . '/../view/application/index/forums.phtml',
+			'application/index/dl' => __DIR__ . '/../view/application/index/dl.phtml',
+			
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
--- library/module/Application/src/Application/Controller/IndexController.php
+++ library/module/Application/src/Application/Controller/IndexController.php
@@ -11,11 +11,51 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Mvc\Router\RouteMatch;
+
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+		return new ViewModel();
+    }
+	
+	public function topicAction()
+    {
+		return new ViewModel();
+    }
+	
+	public function forumsAction()
+    {
         return new ViewModel();
     }
+	
+	public function categoryAction()
+    {
+        return new ViewModel();
+    }
+	
+	public function usersAction()
+    {
+        return new ViewModel();
+    }	
+	
+	public function settingsAction()
+    {
+        return new ViewModel();
+    }
+	
+	public function dlAction()
+    {
+        return new ViewModel();
+    }
+	
+	public function trackerAction()
+    {
+        return new ViewModel();
+    }
+	
+	
+	
 }
